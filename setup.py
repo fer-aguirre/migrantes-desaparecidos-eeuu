b2749bf1a855ba64aad6a7970f0c2cc86954760a
Fer Aguirre <kay_op@protonmail.com>
2023-02-08 18:23:24 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Migrantes Desaparecidos EEUU',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Analysis of data on missing migrants en route to the U.S.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
